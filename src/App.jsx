39667784139ef5b066b01debb81f300d064bb8fd
Monchekin <barthelsonlina@gmail.com>
2024-03-14 12:08:56 +0100

    First Commit React-vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+	return (
+		<>
+			Hej från APP!
+			<Routes>
+				<Route />
+				<Route />
+				<Route />
+			</Routes>
+		</>
+	);
+}
+
+export default App;
