04c5c8cd54b095b3be1e1c6707df7da8c49270bd
Musa Vakilov <dino_crizis@yahoo.com>
2024-03-25 10:56:22 +0100

    klart med bid komponenten

--- src/components/Auktion.jsx
+++ src/components/Auktion.jsx
@@ -1,35 +1,46 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import Bid from './Bid';
+
 const Auktion = () => {
 	const [auctions, setAuctions] = useState(null);
+	const [selectedAuctionId, setSelectedAuctionId] = useState(null);
 
+	const handleBidButtonClick = (auctionId) => {
+		setSelectedAuctionId(auctionId); // Uppdatera selectedAuctionId för att visa budkomponenten
+	};
+
+	// Fetch auktioner och rendera dem
 	useEffect(() => {
-		const getAuctions = () => {
-			fetch('https://auctioneer.azurewebsites.net/auction/j5t')
-				.then((res) => res.json())
-				.then((data) => setAuctions(data));
+		const getAuctions = async () => {
+			try {
+				const response = await fetch('https://auctioneer.azurewebsites.net/auction/j5t');
+				if (!response.ok) {
+					throw new Error('Något gick fel vid hämtning av auktioner');
+				}
+				const data = await response.json();
+				setAuctions(data);
+			} catch (error) {
+				console.error(error);
+			}
 		};
-
 		getAuctions();
-
-		return () => {
-			// TODO: städa upp här
-		};
 	}, []);
 
 	return (
 		<div>
+			<h2>Auktioner</h2>
 			<ul>
 				{auctions &&
 					auctions.map((auction) => (
-						<li>
+						<li key={auction.AuctionID}>
 							<p>{auction.Title}</p>
 							<p>{auction.Description}</p>
-							{/* Split datestring maybe? */}
 							<p>{auction.StartDate}</p>
 							<p>{auction.EndDate}</p>
 							<p>{auction.StartingPrice}</p>
 							<p>{auction.CreatedBy}</p>
-							<hr />
+							<button onClick={() => handleBidButtonClick(auction.AuctionID)}>Lägg bud</button>
+							{selectedAuctionId === auction.AuctionID && <Bid auctionId={auction.AuctionID} />}
 						</li>
 					))}
 			</ul>
