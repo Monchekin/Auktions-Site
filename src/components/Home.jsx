c278e417d35896069ab923f5f72f9a70fff04e02
Hassvanee <hassvan_ee@hotmail.com>
2024-03-30 22:36:30 +0100

    fixat deleteButton

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
 const Home = ({ auctions, setAuctions }) => {
 	const navigate = useNavigate();
@@ -40,6 +40,15 @@ const Home = ({ auctions, setAuctions }) => {
 		return date.toLocaleDateString('sv-SE');
 	};
 
+	const handleOnDelete = (auctionId, e) => {
+		e.stopPropagation(); // Stop event propagation
+		// Perform delete operation based on auctionId
+		console.log('Delete auction with ID:', auctionId);
+		// Filter out the auction with the specified AuctionID
+		const updatedAuctions = auctions.filter((auction) => auction.AuctionID !== auctionId);
+		setAuctions(updatedAuctions);
+	};
+
 	return (
 		<div
 			style={{
@@ -47,14 +56,16 @@ const Home = ({ auctions, setAuctions }) => {
 				flexWrap: 'wrap',
 				gap: '1rem',
 				marginTop: '2vw'
-			}}>
+			}}
+		>
 			<div
 				style={{
 					display: 'flex',
 					flexWrap: 'wrap',
 					gap: '1rem',
 					width: 'calc(80% - 1rem)' // 75% av bredden för att ha 3 kort bredvid varandra med en rems gap
-				}}>
+				}}
+			>
 				{auctions ? (
 					auctions.map((auction) => (
 						<Card
@@ -65,21 +76,26 @@ const Home = ({ auctions, setAuctions }) => {
 								backgroundColor: 'rgb(209, 189, 185)',
 								minWidth: '300px',
 								flexBasis: '300px'
-							}}>
+							}}
+						>
 							<Card.Body>
 								<Card.Title>
-									<p>
-										<b>
-											<u>{auction.Title}</u>
-										</b>
-									</p>
+									<div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+										<div>
+											<b>
+												<u>{auction.Title}</u>
+											</b>
+										</div>
+										<div>
+											<Button variant="danger" onClick={(e) => handleOnDelete(auction.AuctionID, e)}>x</Button>
+										</div>
+									</div>
 								</Card.Title>
 								<Card.Text>
 									<span>{auction.Titel}</span>
 									<span>
 										<b>Startpris: </b>
-										<span
-											style={{ color: 'darkred', textDecoration: 'underline' }}>
+										<span style={{ color: 'darkred', textDecoration: 'underline' }}>
 											{auction.StartingPrice} kr
 										</span>
 									</span>
@@ -99,13 +115,15 @@ const Home = ({ auctions, setAuctions }) => {
 			<div
 				style={{
 					width: 'calc(20% - 1rem)'
-				}}>
+				}}
+			>
 				<Card
 					style={{
 						backgroundColor: 'lightgray',
 						minWidth: '300px',
 						flexBasis: '100%'
-					}}>
+					}}
+				>
 					<Card.Body>
 						<Card.Title>
 							<h5>Klara auktioner:</h5>
